2da7c1666efd278387ceea5ce26cf25038a3cc7e
mattnorthcutt <matt_northcutt@icloud.com>
2025-03-15 08:41:35 -0500

    finished with styling and fixed a bug with my styling and i also finished my read me and did all the requirements!

--- components/loginButton.js
+++ components/loginButton.js
@@ -2,7 +2,7 @@ import { signIn } from '../utils/auth';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
+  const domString = '<h2 class="mb-4" style="color: black;" >WELCOME TO YOUR OWN PERSONAL TECH AND LANGUAGE VOCAB CARDS!</h2> <h3 class="mb-4" style="color: black;"> LOGIN TO GET STARTED!</h3> <button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
   document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
